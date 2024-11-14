40799f34d898292fa5397aae7722c500e0b43111
andyprokop <98879462+andyprokop@users.noreply.github.com>
2024-11-14 12:41:23 +0100

    dz2.2.2

--- dz2.2.py
+++ dz2.2.py
@@ -4,5 +4,5 @@ two = (x % 10000) // 1000
 three = (x % 1000) // 100
 four = (x % 100) // 10
 five = x % 10
-
-print(five,four,three,two,a)
+reversed_num = five*10000 + four*1000 + three*100 + two* 10 + a
+print(reversed_num)
