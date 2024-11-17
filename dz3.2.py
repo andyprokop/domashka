e9a826fdb0ac332f8c63aae62da03b401507f864
andyprokop <98879462+andyprokop@users.noreply.github.com>
2024-11-17 10:31:06 +0100

    dz3.3

--- dz3.2.py
+++ dz3.2.py
@@ -3,9 +3,12 @@ from numpy.ma.core import append
 #=> [10, 12, 3, 4]
 #lst1 = [1] #=> [1]
 #lst1 = [] #> []
-lst1 = [12, 3, 4, 10, 8]
-x = lst1[-1]
-lst1.insert(0, x)
-del (lst1[-1])
+#lst1 = [12, 3, 4, 10, 8]
+if lst1 != []:
+    x = lst1[-1]
+    lst1.insert(0, x)
+    del (lst1[-1])
+    print(lst1)
+else:
+    print(lst1)
 #=> [8, 12, 3, 4, 10]
-print(lst1)
