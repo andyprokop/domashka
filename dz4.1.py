e57a8f22457f729dd64963866473b3cb338ec84e
andyprokop <98879462+andyprokop@users.noreply.github.com>
2024-11-18 16:32:40 +0100

    dz4.1

--- dz4.1.py
+++ dz4.1.py
@@ -1,13 +1,16 @@
-l = [0, 1, 0, 12, 3] #-> [1, 12, 3, 0, 0]
+#l = [0, 1, 0, 12, 3] #-> [1, 12, 3, 0, 0]
 #l = [0] #-> [0]
 #l = [1, 0, 13, 0, 0, 0, 5] #-> [1, 13, 5, 0, 0, 0, 0] (!!!)
-#l = [9, 0, 7, 31, 0, 45, 0, 45, 0, 45, 0, 0, 96, 0] #-> [9, 7, 31, 45, 45, 45, 96, 0, 0, 0, 0, 0, 0, 0]
+l = [9, 0, 7, 31, 0, 45, 0, 45, 0, 45, 0, 0, 96, 0] #-> [9, 7, 31, 45, 45, 45, 96, 0, 0, 0, 0, 0, 0, 0]
 new_l = []
-for i in l:
-    if i == 0 and i < len(l):
-        l.remove(i)
-        new_l.append(i)
-print(l)
+zeros = []
+for i in range(len(l)):
+    if l[i] != 0 and i < len(l):
+        new_l.append(l[i])
+    else:
+        zeros.append(l[i])
+
 print(new_l)
-lst = l + new_l
+print(zeros)
+lst = new_l + zeros
 print(lst)
