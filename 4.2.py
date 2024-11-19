339a52855888f709a45982e362fad0edf7b8c599
andyprokop <98879462+andyprokop@users.noreply.github.com>
2024-11-19 09:15:48 +0100

    dz4.2

---
+++ 4.2.py
@@ -0,0 +1,29 @@
+l = [6] #=> 30
+#[1, 3, 5] => 30
+#[6] => 36
+#[] => 0
+if l == []:
+    print (0)
+else:
+    new_l = []
+    x = 0
+
+    for i in l:
+        if x % 2 == 0:
+            new_l.append(i)
+        x += 1
+        y = sum(new_l)
+        v = y * (new_l[-1])
+    print(v)
+
+
+    #elif l[i] < 0:
+      #  print([])
+    #elif l[i] == 0:
+      #  print(i*i)
+
+
+#l = [6] #=> 36
+#[] #=> 0
+#for i in l:
+    #if i // 2
