d69682e008b60f973c709f9e24e9efdc89fe8372
andyprokop <98879462+andyprokop@users.noreply.github.com>
2024-11-22 11:59:52 +0100

    dz5.2

--- 5.2.py
+++ 5.2.py
@@ -1,6 +1,6 @@
 from yaml import YAMLObject
 
-x = input(str('Do you want to continue? Print y/n: '))
+x = 'y'
 
 while x == 'y':
     a = int(input("Type first figure: "))
@@ -8,25 +8,21 @@ while x == 'y':
     b = int(input("Type second figure: "))
     if c == '+':
        print(a+b)
-       x = input(str('Do you want to continue? Print y/n: '))
 
     elif c == '-':
        print(a-b)
-       x = input(str('Do you want to continue? Print y/n: '))
 
     elif c == '*':
        print(a*b)
-       x = input(str('Do you want to continue? Print y/n: '))
 
     elif c == '/' and  b != 0:
        print(a/b)
-       x = input(str('Do you want to continue? Print y/n: '))
 
     elif c == '/' and  b == 0:
         print("Try numbers that are not equal to zero")
-        x = input(str('Do you want to continue? Print y/n: '))
+
 
     else:
         break
-
+    x = input(str('Do you want to continue? Print y/n: '))
 
