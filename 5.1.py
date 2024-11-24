a1ed7fd0334205334018178c49406779978328df
andyprokop <98879462+andyprokop@users.noreply.github.com>
2024-11-24 21:22:03 +0100

    dz5.1

--- 5.1.py
+++ 5.1.py
@@ -20,19 +20,19 @@ my_string_new = my_string.split("_")
 x = len(my_string_new)
 #print(x)
 if my_string == ('_'):
-    print ('True')
+    print (True)
 elif my_string.islower() != True:
-    print ('False')
+    print (False)
 elif my_string.find('0') == 0 or my_string.find('1') == 0 or my_string.find('2') == 0 or my_string.find('3') == 0 or my_string.find('4') == 0 or my_string.find('5') == 0 or my_string.find('6') == 0 or my_string.find('7') == 0 or my_string.find('8') == 0 or my_string.find('9') == 0:
-    print ('False')
+    print (False)
 elif any(word in words for word in my_string_new) and x <= 1:
-    print ('False')
+    print (False)
 elif any(character in char for character in my_string):
-    print ('False')
+    print (False)
 #elif my_string.find('_') >= 0:
  #   print ('True')
 #elif my_string.isalnum() != True:
   #  print ('False')
 else:
-    print ("True")
+    print (True)
 
