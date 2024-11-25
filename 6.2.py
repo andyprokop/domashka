dc889840f1ee3d1903d946b5b4ec0c295e82afaf
andyprokop <98879462+andyprokop@users.noreply.github.com>
2024-11-25 22:35:04 +0100

    dz6.2

--- 6.2.py
+++ 6.2.py
@@ -1,38 +1,40 @@
 x = int(input('Type here: '))
 
-number_of_day_seconds = 24*60*60
+number_of_day_seconds = 86400
 number_of_hour_seconds = 3600
 number_of_minute_seconds = 60
 
-days = x // number_of_day_seconds
-after_days = x % number_of_day_seconds
+if 0 <= x <= 8640000:
+    days = x // number_of_day_seconds
+    after_days = x % number_of_day_seconds
 
-hours = after_days // number_of_hour_seconds
-after_hours = after_days % number_of_hour_seconds
+    hours = after_days // number_of_hour_seconds
+    after_hours = after_days % number_of_hour_seconds
 
-minutes = after_hours // number_of_minute_seconds
-seconds = after_hours % number_of_minute_seconds
+    minutes = after_hours // number_of_minute_seconds
+    seconds = after_hours % number_of_minute_seconds
+    if days % 10 == 2 or days % 10 == 3 or days % 10 == 4 and days != 11 and days != 14:
+        print(days,'дні,','',end='')
+    elif days % 10 == 1 and days != 11:
+        print(days,'день,','',end='')
+    else:
+        print(days, 'днів,','',end='')
 
-if days % 10 == 2 or days % 10 == 3 or days % 10 == 4 and days != 11 and days != 14:
-    print(days,'дні,','',end='')
-elif days % 10 == 1 and days != 11:
-    print(days,'день,','',end='')
+    if hours != 0:
+        print(hours,":",sep = '', end='')
+    elif hours == 0:
+        print(hours,'0',":",sep = '', end='')
+
+    if minutes != 0:
+        print(minutes,":",sep = '',end='')
+    if minutes == 0:
+        print(minutes,'0',":",sep = '',end='')
+    if seconds != 0:
+        print(seconds)
+    if seconds == 0:
+        print(seconds,'0',sep = '')
 else:
-    print(days, 'днів,','',end='')
-
-if hours != 0:
-    print(hours,":",sep = '', end='')
-elif hours == 0:
-    print(hours,'0',":",sep = '', end='')
-
-if minutes != 0:
-    print(minutes,":",sep = '',end='')
-if minutes == 0:
-    print(minutes,'0',":",sep = '',end='')
-if seconds != 0:
-    print(seconds)
-if seconds == 0:
-    print(seconds,'0',sep = '')
+    print("Error: Enter a valid number (0 to 8640000)")
 #print(after_days)
 
 
