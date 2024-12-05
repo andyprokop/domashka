c4b4996bf0270b751463c1159e1f94863e5173ff
andyprokop <98879462+andyprokop@users.noreply.github.com>
2024-12-05 17:45:50 +0100

    dz6.1

---
+++ 6.1.py
@@ -0,0 +1,25 @@
+#"a-c" -> abc
+#"a-a" -> a
+#"s-H" -> stuvwxyzABCDEFGH
+#"a-A" -> abcdefghijklmnopqrstuvwxyzA
+
+#first = input("Please enter a letter: ")
+#second = input("Please enter another letter: ")
+#count = ord(first)
+#while count <= ord(second):
+   # print(chr(count),end='')
+   # count = count + 1
+
+import string
+
+
+x = string.ascii_letters
+#print(x)
+inp = input("please type: ")
+
+y,z = inp.split('-')
+first_index = x.index(y)
+second_index = x.index(z)
+
+result = ''.join(x[first_index:second_index + 1])
+print(result)
